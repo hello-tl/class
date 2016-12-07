5dcb6d42e1f6994c054e58d7497c354e86b5d135
hello-tl <13343000479@163.com>
2016-12-07 17:16:16 +0800

    常用操作类库

--- TL_Functions.php
+++ TL_Functions.php
@@ -33,7 +33,7 @@
 //                  奔驰宝马贵者趣，公交自行程序员。
 //                  别人笑我忒疯癫，我笑自己命太贱；
 //                  不见满街漂亮妹，哪个归得程序员？
-class Functions{
+class TL_Functions{
     /**
      * 数组转换对象
      * @param $array 数组
